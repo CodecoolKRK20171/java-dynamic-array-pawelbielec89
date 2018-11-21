e8f163114a95f510fd721e2e059b9ff84994047e
Paul <bielcu@gmail.com>
2018-11-21 20:41:31 +0100

    implement field and constructor

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -2,5 +2,8 @@ package com.codecool.dynamicArrayDojo;
 
 // put your code here!
 public class DynamicIntArray {
-
+    private int[] dynamicArray;
+    public void DinamicIntArray(){
+        dynamicArray = new int[10];
+    }
 }
